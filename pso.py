11d4a67ecf21ad9bab9812f54e2d2d5822e38111
Joanna Kolodziejczyk <jkolodziejczyk@wi.zut.edu.pl>
2022-09-14 12:13:07 +0200

    Correction in PSO: 1. Vectorisation 2. pBest condition added 3. results, chenged

--- pso.py
+++ pso.py
@@ -44,46 +44,45 @@ def pso(d, swarm_size, domain, sets, test): # Hyper-parameter of the algorithm
     old_min, old_max = X_.min(), X_.max()
     X = ((X_ - old_min) / (old_max - old_min)) * (domain[1] - domain[0]) + domain[0]
     # Initial velocity
-    V = np.zeros((swarm_size, d)) # zero
-    # V = ss.norm.rvs(d, swarm_size) * 0.1 #lub losowe z rozkładu normalnego
+    # V = np.zeros((swarm_size, d)) # zero
+    V_max = 1/3 * abs(domain[1] - domain[0])
+    V = np.random.randn(swarm_size, d) * V_max #lub losowe z rozkładu normalnego
 
     # Initial bests
-    pBest = np.zeros(d)
-    pBest_fit = np.ones(swarm_size) * np.inf
-    gBest_fit = np.inf
-    gBest = np.zeros(d)
+    pBest = X
+    pBest_fit = test(X)
+    gBest = X[pBest_fit.argmin()]
+    gBest_fit = pBest_fit.min()
 
-    max_iter = 5 #Ile razy wykona się przemieszczanie się roju
-    results = np.zeros((max_iter,3))
-    sumcorrection = 0
+    max_iter = 5  # !!!!Set this as the parameter in halving!!
+    results = []
 
     # PSO Loop
     for j in range(max_iter):
+        x_corr = 0
+        v_corr = 0
+        V = w * V + phi_p * (pBest - X) + phi_g * (gBest - X)
+        X = X + V
+
+        # position outside the domain - correct moving to the domain edge
+        if ( any(X[(X < domain[0]) | (X > domain[1])])):
+            X[X < domain[0]] = domain[0]
+            X[X > domain[1]] = domain[1]
+            x_corr += len(X[(X < domain[0]) | (X > domain[1])])
+        # max_vel control
+        if( any(V[(V < -V_max) | (V > V_max)])):
+            V[V < -V_max] = -V_max
+            V[V > V_max] = V_max
+            v_corr += len(V[(V < -V_max) | (V > V_max)])
+
+        # personal best setup
+        fit = test(X)
+        pBest[(pBest_fit >= fit), :] = X[(pBest_fit >= fit), :]
+        pBest_fit = np.array([pBest_fit, fit]).min(axis=0)
+
+        # global best
+        gBest = pBest[pBest_fit.argmin()]
         gBest_fit = pBest_fit.min()
-        gBest = X[pBest_fit.argmin()]
-        
-        #obliczanie wartości cząstki wg zadanej funkcji
-        pBest_fit = test(X)
-        pBest = X[pBest_fit.argmin()]
 
-        for i in range(swarm_size):
-            V[i] = w * V[i] + phi_p * np.random.rand(1,d).dot(pBest - X[i]) + phi_g * np.random.rand(1,d).dot(gBest - X[i])
-            for n in range(d):
-                if V[i][n] > 1/3 * abs(domain[1] - domain[0]):
-                    V[i][n] = 1/3 * abs(domain[1] - domain[0])
-            # sprawdzenie rozpędzającej się cząstki i ustawić na wartość
-            # max_vel
-            X[i] = X[i] + V[i] 
-
-            for n in range(d):
-                if X[i][n] < domain[0]:
-                    X[i][n] = domain[0]
-                    sumcorrection += 1
-                if X[i][n] > domain[1]:
-                    X[i][n] = domain[1]
-                    sumcorrection += 1
-            # kontrola położenia cząstki w założonej dziedzinie, jeżeli poza,
-            # to ustawić na wartość odpowiednio min lub max
-
-        results[j] = [gBest_fit, max_iter, sumcorrection]
+        results.append([gBest, gBest_fit, np.average(fit), x_corr, v_corr])
     return X, results
