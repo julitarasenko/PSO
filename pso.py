844f92353c77648d59608195e8cd60bc8f2807e5
Julia Tarasenko <ty46715@zut.edu.pl>
2023-05-19 12:52:31 +0200

    Change of parameter initialization place

--- pso.py
+++ pso.py
@@ -15,16 +15,7 @@ import numpy as np
 import math
 
 def pso(d, swarm_size, domain, sets, sets_j, qmc_interval, test, max_iter, exp_min, iftest): # Hyper-parameters of the algorithm
-    # Get all sets
-    # If test the touple length and based on this the random function param are set up
-
-    # print("d, swarm_size, domain, sets, test: ", d, swarm_size, domain, sets, test)
-    
-    w = sets[1].rvs(size=1)
-    phi_p = sets[2].rvs(size=d)
-    phi_g = sets[3].rvs(size=d)
-
-    # # Create particles inside the range
+    # Create particles inside the range
     if (sets_j < qmc_interval[0] or sets_j > qmc_interval[1]):
         X_ = sets[0].rvs(size=(swarm_size, d))
     else:
@@ -51,7 +42,7 @@ def pso(d, swarm_size, domain, sets, sets_j, qmc_interval, test, max_iter, exp_m
     for j in range(max_iter):
         x_corr = 0
         v_corr = 0
-        V = w * V + phi_p * (pBest - X) + phi_g * (gBest - X)
+        V = sets[1].rvs(size=1) * V + sets[2].rvs(size=d) * (pBest - X) + sets[3].rvs(size=d) * (gBest - X)
         X = X + V
 
         # position outside the domain - correct moving to the domain edge
