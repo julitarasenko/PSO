cc0d7696b76d7f158d9842df92274cc79377fad8
Julia Tarasenko <ty46715@zut.edu.pl>
2023-06-04 01:26:23 +0200

    Chang work with phi distributions

--- results_analysis.py
+++ results_analysis.py
@@ -13,20 +13,19 @@ def create_table(criteria):
     results_idx = 0
     for i1 in criteria['function_name']:
         for i2 in criteria['swarm_distribution']:
-            for i3 in criteria['phi_p_distribution']:
-                for i4 in criteria['phi_g_distribution']:
-                    for i5 in criteria['experiment']:
-                        criteria_arr = [i1, i2, i3, i4, i5]
-                        for file in files:
-                            file_name = os.path.basename(file)
-                            name_without_extension = os.path.splitext(file_name)[0]
-                            part_name = name_without_extension.split("_")
-                            for i in range(len(criteria_arr)):
-                                if criteria_arr[i] is not None:
-                                    if part_name[i] == criteria_arr[i]:
-                                        results.append(pd.read_csv(file))
-                                        results[results_idx]['Criteria'] = part_name[i]
-                                        results_idx += 1
+            for i3 in criteria['phi_distribution']:
+                for i4 in criteria['experiment']:
+                    criteria_arr = [i1, i2, i3, i3, i4]
+                    for file in files:
+                        file_name = os.path.basename(file)
+                        name_without_extension = os.path.splitext(file_name)[0]
+                        part_name = name_without_extension.split("_")
+                        for i in range(len(criteria_arr)):
+                            if criteria_arr[i] is not None:
+                                if part_name[i] == criteria_arr[i]:
+                                    results.append(pd.read_csv(file))
+                                    results[results_idx]['Criteria'] = part_name[i]
+                                    results_idx += 1
     return pd.concat(results)
 
 def define_func_parameters(param_ftest):
@@ -61,13 +60,10 @@ def define_dist_swarm_parameters(swarm_dist):
     return swarm_dist_all, swarm_dist_ssqmc, swarm_dist_ss
 
 def define_dist_phi_parameters(generator_set):
-    phi_p_dist_all = [] 
-    phi_g_dist_all = []
-    for phi_p in generator_set['phi_p']:
-        phi_p_dist_all.append(phi_p.dist.name)
-    for phi_g in generator_set['phi_g']:
-        phi_g_dist_all.append(phi_g.dist.name)
-    return phi_p_dist_all, phi_g_dist_all
+    phi_dist_all = [] 
+    for phi in generator_set['phi_p']:
+        phi_dist_all.append(phi.dist.name)
+    return phi_dist_all
 
 def calculate_statistics(df):
     statistics = df.describe()
@@ -80,8 +76,20 @@ def create_boxplot(df, criteria):
         if value != [None]:
             title_parts.append(f"{key}")
     title = ', '.join(title_parts)
+
+    if criteria['phi_distribution'] is not None:
+        criteria_list = np.array(df['Criteria'].unique())
+        index_list = []
+        for i in range(len(criteria_list)):
+            i += 1
+            index_list.append(i)
+            
+        
     for column in ['Mean_best', 'Std_best', 'Mean_f', 'Mean_acc', 'Std_scc', 'Max_acc', 'Max Iteration', 'Time']:
-        df.boxplot(by = 'Criteria', column = column)  
+        df.boxplot(by = 'Criteria', column = column) 
+        if criteria['phi_distribution'] is not None:
+            plt.xticks(index_list, index_list)
+            plt.gcf().set_size_inches(20, 5)
         plt.title(column)
         plt.savefig(f'{title}_{column}.png')
 
@@ -99,13 +107,12 @@ if __name__ == "__main__":
 
     func_name_all, func_unimodal, func_multimodal, func_separable, func_nonseparable = define_func_parameters(param_ftest)
     swarm_dist_all, swarm_dist_ssqmc, swarm_dist_ss = define_dist_swarm_parameters(generator_set['swarm'])
-    phi_p_dist_all, phi_g_dist_all = define_dist_phi_parameters(generator_set)
+    phi_dist_all = define_dist_phi_parameters(generator_set)
 
     criteria = {
         'function_name': None,
-        'swarm_distribution': swarm_dist_all,
-        'phi_p_distribution': None,
-        'phi_g_distribution': None,
+        'swarm_distribution': None,
+        'phi_distribution': phi_dist_all,
         'experiment': None,
             }
 
